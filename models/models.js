0e89fccd4a87969b745c6ecf97a3458051f4dc38
Alexander Trujilo Delgado <Alex@MacBook-Pro-de-Alexander.local>
2015-06-16 14:48:34 +0100

    Despliegue de la BD en Heroku
    Se ha instalado el paquete postgress y se ha preparado el proyecto para
    usar las bases de datos tanto en heroku como en local.

--- models/models.js
+++ models/models.js
@@ -1,12 +1,31 @@
 var path = require('path');
 
+//Postgres DATABASE_URL = postgres://user:password@host:port/database
+//postgres://tinbslbvkcetnf:rnlgncVPjCVLcUBwZBeEUFRnHw@ec2-54-204-20-209.compute-1.amazonaws.com:5432/d6e7jru9414t30
+//SQLite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
 //Cargar modelo ORM
 var Sequelize = require('sequelize');
 
-//Usar BBDD SQLite
-var sequelize = new Sequelize(null, null, null,
-                      {dialect: "sqlite", storage: "quiz.sqlite"}
-                );
+//Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage, //solo SQLite(.env)
+    omitNull: true    //solo Postgres
+  }
+);
 
 //Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
