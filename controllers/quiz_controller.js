84bbb009db7ef58fc46a7b6678c8276bedc2b70f
Alexander Trujilo Delgado <Alex@MacBook-Pro-de-Alexander.local>
2015-06-19 13:45:21 +0100

    AutoLoad
    Se ha añadido una función de Load que factoriza el código de todos las
    peticiones GET que incluyen el parámetro “quizId”.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,9 +1,22 @@
 var models = require('../models/models.js');
 
+//Autoload - Factoriza el código si la ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(function(quiz) {
+    if(quiz){
+      req.quiz = quiz;
+      next();
+    } else {
+      next(new error('No existe un Quiz con quizId = ' + quizId));
+    }
+  }
+).catch(function(error) { next(error);});
+};
+
 //GET /quizes/:id
 exports.show = function(req, res) {
   models.Quiz.find(req.params.quizId).then(function(quiz) {
-    res.render('quizes/show', {quiz: quiz});
+    res.render('quizes/show', {quiz: req.quiz});
   })
 };
 
@@ -11,10 +24,10 @@ exports.show = function(req, res) {
 exports.answer = function(req, res) {
   models.Quiz.find(req.params.quizId).then(function(quiz) {
     var resp = (req.query.respuesta || "");
-    if(resp.toUpperCase() === quiz.respuesta){
-      res.render('quizes/answer', {quiz: quiz, respuesta: 'correcta.'});
+    if(resp.toUpperCase() === req.quiz.respuesta){
+      res.render('quizes/answer', {quiz: req.quiz, respuesta: 'correcta.'});
     } else {
-      res.render('quizes/answer', {quiz: quiz, respuesta: 'incorrecta.'});
+      res.render('quizes/answer', {quiz: req.quiz, respuesta: 'incorrecta.'});
     }
   })
 };
@@ -23,5 +36,6 @@ exports.answer = function(req, res) {
 exports.index = function(req, res) {
   models.Quiz.findAll().then(function(quizes) {
     res.render('quizes/index.ejs', {quizes: quizes});
-  })
+    }
+  ).catch(function(error) { next(error);})
 };
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,18 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+/* Página de entrada (home page). */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Página del autor
 router.get('/author', function(req, res) {
   res.render('author');
 });
+
+//Autoload de comandos que contengan quizId
+router.param('quizId', quizController.load); //Autoload :quizId
 
 //Definición de las rutas de los quizes
 router.get('/quizes',                      quizController.index);
